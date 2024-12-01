2059f5dff42a582e3033aab811db0c652e72682e
mhabibi <mhabibi@leyton.com>
2024-12-01 22:20:41 +0100

    wip - implementing functions to get comment and some info from the submissions

--- PlatformsModels/RedditModel.py
+++ PlatformsModels/RedditModel.py
@@ -13,3 +13,27 @@ class RedditModel:
         )
 
         self.client = client
+    def get_submissions_content(self, submission: Submission) -> dict:
+        return {
+            "id": submission.id,
+            "title": submission.title,
+            "permalink": submission.permalink,
+            "created_date": submission.created_utc,
+            "comments": self.get_submission_comments(submission)
+        }
+
+    @staticmethod
+    def get_submission_comments(submission: Submission) -> list[dict]:
+        wanted_comments = []
+
+        for comment in submission.comments:
+            if isinstance(comment, MoreComments) or len(comment.body.split()) > 100:
+                continue
+            else:
+                wanted_comments.append({
+                    "id": comment.id,
+                    "body": comment.body,
+                })
+
+        return wanted_comments
+
