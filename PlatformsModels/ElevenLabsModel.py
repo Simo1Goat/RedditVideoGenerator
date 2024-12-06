a220d43cfa78c67ff0c27ccabc3557572962d261
mhabibi <medhabi103@gmail.com>
2024-12-06 23:12:23 +0100

    feat - class to transform text to speech using elevenlabs api

---
+++ PlatformsModels/ElevenLabsModel.py
@@ -0,0 +1,71 @@
+import requests
+from random import choice
+from config import ELEVENLABS_URL, XI_API_KEY
+
+
+class ElevenLabs:
+    voices_url = f"{ELEVENLABS_URL}/voices"
+    tts_url = f"{ELEVENLABS_URL}/text-to-speech"
+    current_voices = []
+
+    def list_all_voices(self):
+        headers = {
+            "Accept": "application/json",
+            "xi-api-key": XI_API_KEY,
+            "Content-Type": "application/json"
+        }
+
+        response = requests.get(self.voices_url, headers=headers)
+
+        if response.ok:
+            data_voices = response.json()
+
+            self.current_voices = data_voices["voices"]
+            return 200
+
+        return None
+
+    def text_to_speech(self, text_to_speak: dict):
+        random_voice = choice(self.current_voices)
+        self.tts_url += f"/{random_voice.get('voice_id')}/stream"
+        headers = {
+            "Accept": "application/json",
+            "xi-api-key": XI_API_KEY
+        }
+
+        payload = {
+            "text": text_to_speak.get("body"),
+            "model_id": "eleven_multilingual_v2",
+            "voice_settings": {
+                "stability": 0.5,
+                "similarity_boost": 0.8,
+                "style": 0.0
+            }
+        }
+
+        response = requests.post(self.tts_url,
+                                 headers=headers,
+                                 json=payload,
+                                 stream=True)
+
+        if response.ok:
+            output_file = f"../tmp/voiceover/comment-{text_to_speak.get('id')}.mp3"
+            with open(output_file, mode="wb") as audio_file:
+                for chunk in response.iter_content(chunk_size=1024):
+                    audio_file.write(chunk)
+            print("The audio file is saved successfully :)")
+        else:
+            print(response.text)
+
+
+if __name__ == '__main__':
+    eleven = ElevenLabs()
+
+    eleven.list_all_voices()
+
+    text = {
+        "id": "m05vlxr",
+        "body": "All unscented soap, laundry soap, face soap and body soap. My skin feels better and when I wear perfume it's not clashing with any other cheaper fragrances."
+    }
+
+    eleven.text_to_speech(text)
--- config.py
+++ config.py
@@ -3,3 +3,6 @@ import os
 CLIENT_ID = os.getenv('CLIENT_ID', 'CLIENT_ID_VALUE')
 SECRET_ID = os.getenv('SECRET_ID', 'SECRET_ID_VALUE')
 APP_NAME = os.getenv('APP_NAME', 'APP_NAME_VALUE')
+
+ELEVENLABS_URL = "https://api.elevenlabs.io/v1"
+XI_API_KEY = "<xi_api_key>"
