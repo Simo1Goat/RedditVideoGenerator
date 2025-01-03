23670d626016f153415c7ec4f415a2a392e1dcf2
mhabibi <mhabibi@leyton.com>
2025-01-03 23:10:33 +0100

    feat - function to pair the screenshots and voice_overs based on their shared id

---
+++ tasks/video_generator.py
@@ -0,0 +1,22 @@
+from moviepy import ImageClip, AudioFileClip
+import os
+
+
+def get_matched_pairs(submission_dir: str) -> list[tuple]:
+    """
+    this function is going to be used to create a tuple between the comment
+    screenshot and its voiceover
+    :param: submission directory
+    :return: tuple(comment_screenshot, voice_over_screenshot)
+    """
+    files = os.listdir(submission_dir)
+    screenshots = {f.split(".")[0]: f for f in files if f.endswith(".png")}
+    voice_overs = {f.split(".")[0]: f for f in files if f.endswith(".mp3")}
+
+    matched_pairs = []
+    for file_id, screen in screenshots.items():
+        if file_id in voice_overs:
+            matched_pairs.append((screen, voice_overs[file_id]))
+
+    return matched_pairs
+
