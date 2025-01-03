0641dd0144a242e772f269cb987446c76fee27c1
mhabibi <mhabibi@leyton.com>
2025-01-04 00:04:04 +0100

    feat - function to generate the full video based on the fetched items from reddit submission

--- tasks/video_generator.py
+++ tasks/video_generator.py
@@ -1,5 +1,42 @@
-from moviepy import ImageClip, AudioFileClip
+from moviepy import ImageClip, AudioFileClip, concatenate_videoclips, CompositeVideoClip, VideoFileClip
 import os
+from random import choice
+
+def generate_video(submission_id: str):
+    background_vid_dir = f"../tmp/backgrounds/"
+    submission_dir = f"../tmp/submission_{submission_id}"
+    clips = []
+
+    if os.path.exists(submission_dir):
+        matched_pairs = get_matched_pairs(submission_dir)
+        title_files = [item for item in matched_pairs if item[0].startswith("title")][0]
+        comment_files = [item for item in matched_pairs if item[0].startswith("comment")]
+        # the beginning of the clip
+        title_clip = create_clip(title_files[0], title_files[1])
+        clips.append(title_clip)
+
+        for comment in comment_files:
+            comment_clip = create_clip(comment[0], comment[1])
+            clips.append(comment_clip)
+
+    title_and_comment_clips = concatenate_videoclips(clips)
+
+    # we need to add the background video
+    background_vids = os.listdir(background_vid_dir)
+    background_vid = choice(background_vids)
+    background_vid_path = os.path.join(background_vid_dir, background_vid)
+    background_clip = VideoFileClip(filename=background_vid_path).without_audio()
+
+    # create a composite clip
+    clips = [background_clip, title_and_comment_clips]
+    final_clip = CompositeVideoClip(clips)
+    output_file = os.path.join(submission_dir, f"final-{submission_id}.mp4")
+    final_clip.write_videofile(output_file,
+                               codec="mpeg4",
+                               threads=12,
+                               bitrate="8000k",
+                               logger="bar")
+
 
 def create_clip(screenShotFile, voiceOverFile):
     audio_clip = AudioFileClip(voiceOverFile)
